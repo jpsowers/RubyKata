d185858c377347f1aa123d688603139a6e4c3e5d
Jim Sowers <jpsowers@gmail.com>
2013-05-30 21:56:16 -0500

    cleaning and adding a todo

--- RubyKata/app/Game_Of_Life.rb
+++ RubyKata/app/Game_Of_Life.rb
@@ -1,5 +1,6 @@
 class Game_Of_Life
   def grid
+    #TODO: add two dimensional string array to hold the grid for now
      'boogers'
   end
 
--- RubyKata/test/game_of_life_tests.rb
+++ RubyKata/test/game_of_life_tests.rb
@@ -3,16 +3,12 @@ require '../app/Game_Of_Life'
 
 class MyTest < Test::Unit::TestCase
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
+
   def setup
 
   end
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
-  def test_game_has_grid_with_nine_squares
+    def test_game_has_grid_with_nine_squares
     game = Game_Of_Life.new
     expected = game.grid
     assert_equal expected, 'boogers'
@@ -20,7 +16,7 @@ class MyTest < Test::Unit::TestCase
   end
 
   def teardown
-    # Do nothing
+
   end
 
 end
